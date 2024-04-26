d0d867eeff37a695b82eb1f843875e98b0d0fe13
Shashank Phatkure <shashankphatkure@gmail.com>
2024-04-26 20:47:41 +0530

    signlanguage

---
+++ app/learn/page.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <div>
+          <iframe src="/learn/index.html" width="100%" height="768"></iframe>
+          <div className="mt-6"></div>
+        </div>
+      </main>
+    </div>
+  );
+}
